7b0d2805dc36cdea980e147dcb544af1a70da6c1
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2024-07-10 07:26:40 +0000

    constainer config done

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all JS code as newer version 
+//alert(3 + 3) // we are using nodejs, not browser
+
+/*console.log(3 
+    + 3) can't write in this formst cause it may alter code readibility */
+
+console.log("hitesh")
+
+let name =" hitesh"
+let age  = 18
+let isLoggedIn = false
+
+//number =>2 to power 53 
+
+// bigInt
+
+// string =>""
+
+// boolean => true/false
+
+//null =>standalone value 
+
+// undefined => 
+
+// symbol =>unique
+
+//object
+
+console.log(typeof "hitesh");
+console.log(typeof null);
+console.log(typeof undefined);
