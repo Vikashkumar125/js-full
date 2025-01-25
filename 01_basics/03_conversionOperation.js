544e25a8f2cc4afc40b357d5ae4da1eecdb83261
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2025-01-25 05:39:19 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,19 @@
+// let score = "33abc"
+
+// console.log(typeof(score));
+// console.log(typeof score);
+// let valueInnumber = Number(score);
+
+
+// console.log(typeof valueInnumber);
+// console.log(valueInnumber)
+// console.log(typeof valueInnumber);
+// "33an"= NaN is not a number its a type of dataType
+
+// let isLoggedIn =0;
+// let booleanIsLoggedIn=Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(Boolean(isLoggedIn));
+
+// 1=>true;  0=>false;
+// ""=> false; "hitesh"=> true;
