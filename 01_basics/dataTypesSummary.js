74cb74d18cae23e8f78ba562a308e1991d4ef005
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2025-01-26 20:53:42 +0000

    dataType Summary

---
+++ 01_basics/dataTypesSummary.js
@@ -0,0 +1,34 @@
+// //Primitive
+
+// // 7 Types: String , Boolean , Number , null ,  undefined , Symbol, BigInt.
+
+// const  score=100;
+// const Score1=100.5; // there is nothing a concept of float and double in javascript.
+
+// let useremail; // undefined 
+// let usereamil=undefined; // undefined 
+
+// const id = Symbol('123');
+// const anotherId = Symbol('123');
+
+// console.log(id===anotherId);
+// console.log(id==anotherId);
+
+const bigNumber = 3245668756543n;
+
+// Refrence Type (Non primitive)
+
+// array  , objects, function 
+
+const hero =["ram" , "shyam", "Hari"];
+
+let obj={
+    name :"umang",
+    age: "29",
+}
+
+const myFunction=function(){
+    console.log("hello wolrd");
+
+} 
+
