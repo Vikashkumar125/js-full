361dc9eb4e17b0ede98fb7e5a563a4e5b5fd5dae
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2025-01-26 20:15:47 +0000

    Comparisions in js

---
+++ 01_basics/04_conversion.js
@@ -0,0 +1,17 @@
+// console.log(null>0); // false 
+// console.log(null==0); //false
+// console.log(null>=0); // true
+ 
+// console.log("2"== 2); // true 
+// console.log(typeof "2"); // String 
+// console.log(typeof 2); // number
+// console.log("2"=== 2); // false cause it's checking datatype 
+
+console.log(undefined ==0);
+console.log(undefined > 0);
+console.log(undefined >= 0);
+
+let val1=undefined;
+console.log(typeof val1);
+console.log(val1);
+ 
