c123c68d8868e00c8b38596f1a8d5eff9cd9a295
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2025-01-27 06:47:57 +0000

    Some Basics of dataTypes and numbers and functions

--- 01_basics/04_conversion.js
+++ 01_basics/04_conversion.js
@@ -14,4 +14,24 @@ console.log(undefined >= 0);
 let val1=undefined;
 console.log(typeof val1);
 console.log(val1);
- 
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2); 
