af63d60b1dff9c83209b899116edd6a9070855f3
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2025-01-28 08:56:28 +0000

    some basics of dates

---
+++ 01_basics/07_datesin.js
@@ -0,0 +1,8 @@
+// console.log(Date);
+// let myCreateDate =new Date(2023, 0, 23)
+
+// let myCreateDate =new Date(2023, 0, 23, 5, 3)
+
+//let myCreateDate =new Date("2023, 3, 23");
+let myCreateDate =new Date("01-14-2024");
+console.log(myCreateDate.toLocaleDateString());
