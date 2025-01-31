ec9ee4d7b72526f21d6943b4b317a15b4e2ff935
Vikash Kumar <91794222+Vikashkumar125@users.noreply.github.com>
2025-01-31 05:29:27 +0000

    Some new features in dates and time

--- 01_basics/07_datesin.js
+++ 01_basics/07_datesin.js
@@ -6,3 +6,16 @@
 //let myCreateDate =new Date("2023, 3, 23");
 let myCreateDate =new Date("01-14-2024");
 console.log(myCreateDate.toLocaleDateString());
+let myTimeStamp= Date.now();
+// console.log(myTimeStamp );
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate =new Date();
+console.log(newDate.getMonth());
+// console.log(newDate.toLocaleDateString());
+ const hello=newDate.toLocaleString('default',{
+    weekday: "long",
+   
+})
+console.log(hello);
